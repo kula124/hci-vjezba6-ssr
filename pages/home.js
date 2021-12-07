e714cc96b29e3547e469cfdd981409f7008ce7e8
kula <kula124@gmail.com>
2021-12-07 22:24:37 +0100

    Setting up static props for home

--- pages/home.js
+++ pages/home.js
@@ -2,16 +2,28 @@ import Header from '@/modules/header/header.js';
 import Cta from '@/modules/cta/cta.js';
 import Testimonials from '@/modules/testimonials/testimonials.js';
 import Footer from '@/modules/footer';
+import BlogPosts from '@/modules/BlogPosts/blogPosts';
 
-const Hello = () => {
+import { getPosts } from 'helper';
+
+const Hello = ({ blogPosts }) => {
     return (
         <>
             <Header />
             <Cta />
             <Testimonials />
+            <BlogPosts posts={blogPosts} />
             <Footer />
         </>
     );
 };
+
+export async function getStaticProps() {
+    const blogPosts = await getPosts();
+
+    return {
+        props: { blogPosts },
+    };
+}
 
 export default Hello;
