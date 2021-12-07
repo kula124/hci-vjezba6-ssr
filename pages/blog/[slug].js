c39cb52603a497898339d34f7379358e10e0a636
kula <kula124@gmail.com>
2021-12-07 21:44:03 +0100

    Created slug

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,8 @@
-const BlogPost = ({ post }) => <div>What to do :O</div>;
-/*(
+import Image from 'next/image';
+
+import { getPostBySlug, getPosts } from 'helper';
+
+const BlogPost = ({ post }) => (
     <main className="w-2/3 mx-auto my-0">
         <h1 className="text-center text-8xl text-gray-800">{post.title}</h1>
         <h2 className="text-center text-5xl my-8 text-gray-600">
@@ -12,6 +15,31 @@ const BlogPost = ({ post }) => <div>What to do :O</div>;
             <span>Learn more</span>
         </a>
     </main>
-);*/
+);
 
 export default BlogPost;
+
+export async function getStaticPaths() {
+    const posts = await getPosts();
+
+    const paths = posts.map((post) => ({
+        params: {
+            slug: post.slug,
+        },
+    }));
+
+    return {
+        paths,
+        fallback: true,
+    };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+    const post = await getPostBySlug(slug);
+
+    return {
+        props: {
+            post,
+        },
+    };
+}
